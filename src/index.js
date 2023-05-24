2fe69f761f461354c493fc3739870f26d766256e
20520846 <90497301+20520846@users.noreply.github.com>
2023-05-24 21:04:30 +0700

    Static file & SCSS

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const { engine } = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+//HTTP logger
 app.use(morgan('combined'));
 
 //Template engine
@@ -14,8 +17,6 @@ app.engine('hbs', engine({
 }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
-
-console.log('PATH: ', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
   res.render('home');
